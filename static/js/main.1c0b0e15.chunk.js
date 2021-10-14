996f83868435602a378058d3fc23004b36c56ac3
Victor Ramirez <vicdeveloper83@gmail.com>
2021-10-14 22:06:38 +0100

    Updates

---
+++ static/js/main.1c0b0e15.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonplawyer=this.webpackJsonplawyer||[]).push([[0],{11:function(e,t,n){"use strict";n.r(t);var c=n(1),r=n.n(c),s=n(3),a=n.n(s),i=(n(8),n(9),n(0));var o=function(){return Object(i.jsx)("div",{className:"App"})};a.a.render(Object(i.jsx)(r.a.StrictMode,{children:Object(i.jsx)(o,{})}),document.getElementById("root"))},8:function(e,t,n){},9:function(e,t,n){}},[[11,1,2]]]);
+//# sourceMappingURL=main.1c0b0e15.chunk.js.map
